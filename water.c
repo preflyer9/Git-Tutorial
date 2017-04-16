4e7eb2fb470cc2e59d62f026cdaa07be29d70d8b
Justin Jonas <jona0043@umn.edu>
2017-04-16 23:53:52 +0000

    added minutes squared with a squared function

--- water.c
+++ water.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
-
+int square(int x);
 /* Converts time in shower to equivilent bottles of drinking water*/
 
 int main(void)
@@ -8,4 +8,13 @@ int main(void)
     printf("Shower length (minutes): ");
     int m = get_int();
     printf("Minutes: %i\nBottles: %i\n", m, m * 12);
+    
+    int x = square(m);
+    printf("x^2 is: %i\n", x);
+    
+}
+
+int square(int x)
+{
+    return x*x;
 }
